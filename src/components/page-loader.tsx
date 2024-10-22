da4c8bf96daf50ee20b872cc5152e95fd6dc66ba
Richard Griffiths <rich.griffiths89@gmail.com>
2024-10-22 23:26:32 +0100

    Removed unused icon

--- src/components/page-loader.tsx
+++ src/components/page-loader.tsx
@@ -1,12 +1,8 @@
-import { Loader } from "lucide-react"
 import { Skeleton } from "./ui/skeleton"
 
 export const PageLoader = () => {
   return (
     <div className="flex flex-col space-y-6">
-      {/* <div className="flex items-center justify-center h-screen">
-        <Loader className="size-6 animate-spin text-muted-foreground" />
-      </div> */}
       <div className="flex items-center gap-x-4">
         <Skeleton className="h-[100px] w-[250px] rounded-xl" />
         <Skeleton className="h-[100px] w-[250px] rounded-xl" />
