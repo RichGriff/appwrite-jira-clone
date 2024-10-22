1440314e577694f60f837a4eb679db680e409a0f
Richard Griffiths <rich.griffiths89@gmail.com>
2024-10-22 11:27:33 +0100

    Task detail page added

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -18,7 +18,11 @@ import { TaskStatus } from "../types"
 import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks"
 import { DataCalendar } from "./data-calendar"
 
-export const TaskViewSwitcher = () => {
+interface TaskViewSwitcherProps {
+  hideProjectFilter?: boolean
+}
+
+export const TaskViewSwitcher = ({ hideProjectFilter } : TaskViewSwitcherProps) => {
   const [{
     status, 
     assigneeId, 
@@ -68,7 +72,7 @@ export const TaskViewSwitcher = () => {
           </Button>
         </div>
         <DottedSeperator className="my-4" />
-          <DataFilters />
+          <DataFilters hideProjectFilter={hideProjectFilter} />
         <DottedSeperator className="my-4" />
         {isLoadingTasks ? (
           <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
